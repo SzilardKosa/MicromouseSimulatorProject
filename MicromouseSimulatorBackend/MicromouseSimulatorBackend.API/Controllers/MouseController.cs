829ce4e0ea6dece1b46a4a6260e1569a875d0564
Szilárd Kósa <kosa.szilard@mirango.hu>
2021-08-27 20:11:37 +0200

    Added basic crud api for mice

---
+++ MicromouseSimulatorBackend/MicromouseSimulatorBackend.API/Controllers/MouseController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MicromouseSimulatorBackend.API.DTOs;
+using MicromouseSimulatorBackend.BLL.Models;
+using MicromouseSimulatorBackend.BLL.ServiceInterfaces;
+using MicromouseSimulatorBackend.BLL.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicromouseSimulatorBackend.API.Controllers
+{
+    [Route("mice")]
+    [ApiController]
+    public class MouseController : ControllerBase
+    {
+        private readonly IMouseService _service;
+        public MouseController(IMouseService service)
+        {
+            this._service = service;
+        }
+
+        // Fetch all mice
+        [HttpGet]
+        public ActionResult<IEnumerable<MouseDTO>> GetMice()
+        {
+            return Ok(_service.FindAll().Select(m => new MouseDTO(m)));
+        }
+
+        // Find one mouse by id
+        [HttpGet("{id}")]
+        public ActionResult<MouseDTO> GetMouse(string id)
+        {
+            var result = _service.FindById(id);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(new MouseDTO(result));
+        }
+
+        // Create a new mouse
+        [HttpPost]
+        public ActionResult<MouseDTO> CreateNewMouse(MouseDTO mouseDTO)
+        {
+            // Handle error if no data is sent.
+            if (mouseDTO == null)
+                return BadRequest("Mouse data must be set!");
+            // Handle error if id is sent.
+            if (mouseDTO.Id != null)
+                return BadRequest("No ID should be provided!");
+
+            // Map the DTO to entity and save the entity
+            Mouse createdEntity = _service.Create(mouseDTO.ToEntity());
+
+            // According to the conventions, we have to return a HTTP 201 created repsonse, with
+            // field "Location" in the header pointing to the created object
+            return CreatedAtAction(
+                nameof(GetMouse),
+                new { id = createdEntity.Id },
+                new MouseDTO(createdEntity));
+        }
+
+        // Update an existing mouse
+        [HttpPut("{id}")]
+        public ActionResult UpdateMouse(string id, MouseDTO mouseDTO)
+        {
+            // Handle error if no data is sent.
+            if (mouseDTO == null)
+                return BadRequest("Mouse data must be set!");
+
+            try
+            {
+                // Map the DTO to entity and save it
+                _service.Update(id, mouseDTO.ToEntity());
+
+                // According to the conventions, we have to return HTTP 204 No Content.
+                return NoContent();
+            }
+            catch (DocumentDoesntExistsException)
+            {
+                // Handle error if the mouse to update doesn't exists.
+                return BadRequest("No Mouse exists with the given ID!");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteMouse(string id)
+        {
+            _service.Delete(id);
+            // According to the conventions, we have to return HTTP 204 No Content.
+            return NoContent();
+        }
+    }
+}
---
+++ MicromouseSimulatorBackend/MicromouseSimulatorBackend.API/DTOs/MouseDTO.cs
@@ -0,0 +1,38 @@
+﻿using MicromouseSimulatorBackend.BLL.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MicromouseSimulatorBackend.API.DTOs
+{
+    public class MouseDTO
+    {
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public bool IsFullSize { get; set; }
+     
+
+        // Because of the way JSON Deserialization works (first an object is created, then
+        // its properties set), we need a default constructor.
+        public MouseDTO()
+        {
+
+        }
+        public MouseDTO(Mouse mouse)
+        {
+            this.Id = mouse.Id;
+            this.Name = mouse.Name;
+            this.IsFullSize = mouse.IsFullSize;
+        }
+
+        public Mouse ToEntity()
+        {
+            return new Mouse()
+            {
+                Id = this.Id,
+                Name = this.Name,
+                IsFullSize = this.IsFullSize,
+            };
+        }
+    }
+}
