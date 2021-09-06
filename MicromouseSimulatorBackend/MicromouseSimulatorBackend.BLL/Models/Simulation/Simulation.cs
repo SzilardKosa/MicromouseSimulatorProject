ebff9b5b583f1a697f4b1158864ee7f21cb574bd
Szilárd Kósa <kosa.szilard@mirango.hu>
2021-09-06 21:21:32 +0200

    Code prettified

--- MicromouseSimulatorBackend/MicromouseSimulatorBackend.BLL/Models/Simulation/Simulation.cs
+++ MicromouseSimulatorBackend/MicromouseSimulatorBackend.BLL/Models/Simulation/Simulation.cs
@@ -1,9 +1,5 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace MicromouseSimulatorBackend.BLL.Models
 {
